7a056d33ea45bb1def2bae5a4bcf49b0964e3d23
Zacky Ma <machi@microsoft.com>
2026-04-03 15:12:56 -0700

    add function to observe body and make sure polyfilled objects are garbage collected

--- packages/focusgroup/src/polyfill.js
+++ packages/focusgroup/src/polyfill.js
@@ -5,8 +5,55 @@ import { FocusGroup } from "./focusgroup.js";
 import { createTreeWalker } from "./shadow-utils/index.js";
 import { hasDocument, supportsFocusGroup } from "./utils.js";
 
+let elementPolyfillMap;
+
+if (
+  hasDocument() &&
+  typeof MutationObserver !== "undefined" &&
+  !supportsFocusGroup()
+) {
+  globalThis.__FOCUSGROUP_POLYFILL_OBSERVE_BODY = false;
+
+  globalThis.__FOCUSGROUP_POLYFILL_ELEMENT_POLYFILL_MAP ??= new Map();
+  /** @type {Map<HTMLElement, FocusGroup>} */
+  elementPolyfillMap = globalThis.__FOCUSGROUP_POLYFILL_ELEMENT_POLYFILL_MAP;
+
+  if (!globalThis.__FOCUSGROUP_POLYFILL_GLOBAL_OBSERVER) {
+    const observer = new MutationObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.type !== "childList") {
+          continue;
+        }
+
+        for (const node of entry.removedNodes) {
+          if (elementPolyfillMap.has(node)) {
+            elementPolyfillMap.get(node).recycle();
+            elementPolyfillMap.delete(node);
+          }
+        }
+
+        if (!globalThis.__FOCUSGROUP_POLYFILL_OBSERVE_BODY) {
+          continue;
+        }
+
+        for (const node of entry.addedNodes) {
+          if (
+            node.nodeType === Node.ELEMENT_NODE &&
+            node.hasAttribute("focusgroup") &&
+            !elementPolyfillMap.has(node)
+          ) {
+            polyfill(node);
+          }
+        }
+      }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+    globalThis.__FOCUSGROUP_POLYFILL_GLOBAL_OBSERVER = observer;
+  }
+}
+
 /**
- * Polyfill the `focusgroup` HTML attribute for the given element and its
+ * Polyfills the `focusgroup` HTML attribute for the given element and its
  * descendants.
  *
  * @param {HTMLElement} root - The polyfill target. Defaults to `<body>`.
@@ -31,6 +78,26 @@ export function polyfill(root) {
   );
 
   do {
-    new FocusGroup(walker.currentNode);
+    const element = walker.currentNode;
+
+    if (elementPolyfillMap.has(element)) {
+      continue;
+    }
+
+    elementPolyfillMap.set(element, new FocusGroup(walker.currentNode));
   } while (walker.nextNode());
+}
+
+/**
+ * Polyfills all potential focusgroups in `document.body`, observes DOM changes,
+ * and polyfills any newly added focusgroups.
+ */
+export function polyfillAllAndObserve() {
+  globalThis.__FOCUSGROUP_POLYFILL_OBSERVE_BODY = true;
+
+  if (!hasDocument() || elementPolyfillMap.has(document.body)) {
+    return;
+  }
+
+  polyfill();
 }
