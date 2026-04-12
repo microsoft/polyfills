f920faa4cb1922fcc6411fed7c4509c2c935f473
Zacky Ma <machi@microsoft.com>
2026-04-12 13:31:31 -0700

    restore nested focusability when focus leaves a group that has nomemory (#34)
    * restore nested focusability when focus leaves a group that has nomemory
    
    * Change files

--- packages/focusgroup/src/focusgroup.js
+++ packages/focusgroup/src/focusgroup.js
@@ -525,6 +525,10 @@ export class FocusGroup {
     this.#start.tabIndex = 0;
     this.#itemWalker.currentNode = this.#start;
 
+    if (focusLeavingGroup) {
+      this.#enableKeyboardFocusabilityForProxyHost(this.#start);
+    }
+
     // Proxy hosts for the reset tab stop are already set above.
 
     flushAllObservers();
