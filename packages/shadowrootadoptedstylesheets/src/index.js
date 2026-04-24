006826b18a885255cd75b66e32427d6df5c55c95
Zacky Ma <machi@microsoft.com>
2026-04-23 17:58:06 -0700

    observe dom changes

--- packages/shadowrootadoptedstylesheets/src/index.js
+++ packages/shadowrootadoptedstylesheets/src/index.js
@@ -20,17 +20,66 @@ function isCSSModule(element) {
 }
 
 /**
- * Whether the given element is a custom element that has the
- * `data-shadowrootadoptedstylesheets` attribute and contains a shadow root.
+ * Whether the given element has the * `data-shadowrootadoptedstylesheets`
+ * attribute and contains a shadow root.
  * @param {Element} element
  * @returns {boolean}
  */
 function hasSpecifier(element) {
-  return !!(
-    element.nodeName.includes("-") &&
-    element.hasAttribute(ATTR_NAME_DATA_BASE) &&
-    element.shadowRoot
-  );
+  return !!(element.hasAttribute(ATTR_NAME_DATA_BASE) && element.shadowRoot);
+}
+
+/**
+ * Processes a given element.
+ * @param {Element} element
+ * @param {Map<string, CSSStyleSheet>} map
+ * @param {Document!} doc
+ */
+function processElement(element, map, doc) {
+  if (isCSSModule(element)) {
+    const specifier = element.getAttribute(ATTR_NAME_SPECIFIER).trim();
+
+    if (!specifier || map.has(specifier)) {
+      return;
+    }
+
+    const sheet = new CSSStyleSheet({ media: element.media });
+    sheet.replaceSync(element.textContent);
+    map.set(specifier, sheet);
+
+    return;
+  }
+
+  if (hasSpecifier(element)) {
+    const attrValue = element.getAttribute(ATTR_NAME_DATA_BASE).trim();
+    const specifiers = attrValue.split(" ");
+    const pending = [];
+    const sheets = specifiers.filter(Boolean).map((s) => {
+      const specifier = s.trim();
+      const sheet = map.get(specifier) ?? new CSSStyleSheet();
+
+      if (!map.has(specifier)) {
+        map.set(specifier, sheet);
+        pending.push(
+          fetch(specifier, { headers: { accept: "text/css" } })
+            .then((res) => (res.ok && res.status === 200 ? res.text() : ""))
+            .then((text) => {
+              sheet.replaceSync(text);
+            }),
+        );
+      }
+
+      return sheet;
+    });
+
+    element.shadowRoot.adoptedStyleSheets.push(...sheets);
+
+    Promise.all(pending).then(() => {
+      element.toggleAttribute(ATTR_NAME_DATA_READY, true);
+    });
+
+    installToRoot(map, doc, element.shadowRoot);
+  }
 }
 
 /**
@@ -52,53 +101,25 @@ function installToRoot(map, doc, root) {
   );
 
   while (walker.nextNode()) {
-    const element = walker.currentNode;
-
-    if (isCSSModule(element)) {
-      const specifier = element.getAttribute(ATTR_NAME_SPECIFIER).trim();
-
-      if (!specifier || map.has(specifier)) {
-        continue;
-      }
-
-      const sheet = new CSSStyleSheet({ media: element.media });
-      sheet.replaceSync(element.textContent);
-      map.set(specifier, sheet);
-
-      continue;
-    }
-
-    if (hasSpecifier(element)) {
-      const attrValue = element.getAttribute(ATTR_NAME_DATA_BASE).trim();
-      const specifiers = attrValue.split(" ");
-      const pending = [];
-      const sheets = specifiers.filter(Boolean).map((s) => {
-        const specifier = s.trim();
-        const sheet = map.get(specifier) ?? new CSSStyleSheet();
-
-        if (!map.has(specifier)) {
-          map.set(specifier, sheet);
-          pending.push(
-            fetch(specifier, { headers: { accept: "text/css" } })
-              .then((res) => (res.ok && res.status === 200 ? res.text() : ""))
-              .then((text) => {
-                sheet.replaceSync(text);
-              }),
-          );
-        }
-
-        return sheet;
-      });
-
-      element.shadowRoot.adoptedStyleSheets.push(...sheets);
-
-      Promise.all(pending).then(() => {
-        element.toggleAttribute(ATTR_NAME_DATA_READY, true);
-      });
-
-      installToRoot(map, doc, element.shadowRoot);
-    }
+    processElement(walker.currentNode, map, doc);
   }
+
+  new MutationObserver((entries) => {
+    const elements = entries
+      .filter((e) => e.addedNodes)
+      .flatMap((e) => [...e.addedNodes])
+      .filter(
+        (n) =>
+          n.nodeType === Node.ELEMENT_NODE &&
+          (isCSSModule(n) || hasSpecifier(n)),
+      );
+    for (const element of elements) {
+      processElement(element, map, doc);
+    }
+  }).observe(root, {
+    childList: true,
+    subtree: true,
+  });
 }
 
 /** @returns {Promise<void>} */
